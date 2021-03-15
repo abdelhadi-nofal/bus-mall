00006e35ee7c300736f959cf2f8a061aac54d610
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-03-15 22:28:13 +0200

    lab-12

--- app.js
+++ app.js
@@ -39,23 +39,39 @@ function randomNumber(min, max) {
 }
 
 function render(){
-  const leftIndex=randomNumber(0,prodcts.all.length-1);
-  const leftRandomprodcts=prodcts.all[leftIndex];
-  leftImage.src=leftRandomprodcts.path;
-  leftImage.title=leftRandomprodcts.name;
-  leftImage.alt=leftRandomprodcts.name;
 
-  const middleIndex=randomNumber(0,prodcts.all.length-1);
-  const middleRandomprodcts=prodcts.all[middleIndex];
-  middleImage.src=middleRandomprodcts.path;
-  middleImage.title=middleRandomprodcts.name;
-  middleImage.alt=middleRandomprodcts.name;
+  do{const leftIndex=randomNumber(0,prodcts.all.length-1);
+    const leftRandomprodcts=prodcts.all[leftIndex];
+    const middleIndex=randomNumber(0,prodcts.all.length-1);
+    const middleRandomprodcts=prodcts.all[middleIndex];
+    const rightIndex=randomNumber(0,prodcts.all.length-1);
+    const rightRandomprodcts=prodcts.all[rightIndex];
 
-  const rightIndex=randomNumber(0,prodcts.all.length-1);
-  const rightRandomprodcts=prodcts.all[rightIndex];
-  rightImage.src=rightRandomprodcts.path;
-  rightImage.title=rightRandomprodcts.name;
-  rightImage.alt=rightRandomprodcts.name;
+    if(leftRandomprodcts!==middleRandomprodcts && leftRandomprodcts!==rightRandomprodcts)
+    {
+      leftImage.src=leftRandomprodcts.path;
+      leftImage.title=leftRandomprodcts.name;
+      leftImage.alt=leftRandomprodcts.name;
+      prodcts.all[leftIndex].views++;
+    }else{
+      break;
+    }
+    if(leftRandomprodcts!==middleRandomprodcts && middleRandomprodcts!==rightRandomprodcts){
+      middleImage.src=middleRandomprodcts.path;
+      middleImage.title=middleRandomprodcts.name;
+      middleImage.alt=middleRandomprodcts.name;
+      prodcts.all[middleIndex].views++;}else{
+      break;
+    }
+
+    if(rightRandomprodcts!==middleRandomprodcts && leftRandomprodcts!==rightRandomprodcts){
+      rightImage.src=rightRandomprodcts.path;
+      rightImage.title=rightRandomprodcts.name;
+      rightImage.alt=rightRandomprodcts.name;
+      prodcts.all[rightIndex].views++;}else{
+      break;
+    }
+  } while (leftImage === middleImage || leftImage ===rightImage || rightImage ===middleImage);
 }
 
 render();
@@ -75,30 +91,75 @@ function clickHandler(event){
         console.table(prodcts.all[i]);
         console.log(prodcts.all[i].votes);
       }
-      if(prodcts.all[i].path === event.target.src || prodcts.all[i].name === event.target.title ){
-        prodcts.all[i].views++;
-        // console.table(prodcts.all[i]);
-        console.log(prodcts.all[i].views);
-      }
     }
     console.log(cnt);
     render();
   }
   if (cnt === rounds) {
     for(let i=0;i<prodcts.all.length;i++){
-      imageSection.removeEventListener('click', clickHandler);
+
       document.getElementById('resultBtn').addEventListener('click', function() {
-        let ul=document.getElementById('demo');
-        let li=document.createElement('li');
-        li.innerText=(prodcts.all[i].name +' had '+prodcts.all[i].votes + ' votes, and was seen  '+ prodcts.all[i].views +' times.');
-        ul.appendChild(li);
+
+        CreateChart();
       });
+      imageSection.removeEventListener('click', clickHandler);
     }}
   cnt++;
+
 }
 
-document.getElementById('resultBtn').addEventListener('click', function() {
-});
+function CreateChart(){
+  let context = document.getElementById('myChart').getContext('2d');
+  let getprodctsNames=[];
+  let getprodctsVotes=[];
+  let getprodctsViews=[];
+
+  for(let i=0;i<prodcts.all.length;i++){
+    getprodctsNames.push(prodcts.all[i].name);
+  }
+  for(let i=0;i<prodcts.all.length;i++){
+    getprodctsVotes.push(prodcts.all[i].votes);
+  }
+  for(let i=0;i<prodcts.all.length;i++){
+    getprodctsViews.push(prodcts.all[i].views);
+  }
+  let chartObject={
+    // The type of chart we want to create
+    type: 'bar',
+    // The data for our dataset
+    data: {
+      labels:getprodctsNames,
+      datasets: [{
+        label: 'prodcts voting results',
+        backgroundColor: 'rgb(100, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: getprodctsVotes ,
+      },{
+        label: 'prodcts views results',
+        backgroundColor: 'rgb(250, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: getprodctsViews ,
+      }],
+    },
+
+    // Configuration options go here
+    options: {
+
+      scales: {
+        xAxes: [{
+          barPercentage: 1
+        }]
+      }
+
+    }
+  };
+
+  let chart = new Chart(context,chartObject);
+}
+
+render();
+
+
 
 
 
